e0550220852d5149795af121b64a8de765986ba9
mohanapriya_g_idp <123936876+Mohanapriya-G-idp@users.noreply.github.com>
2023-01-31 15:37:08 +0530

    Add files via upload

---
+++ FilesAssign.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FilesAssign{
+    public static void main(String[] args) throws IOException {
+
+        File f = new File("a.txt");
+
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("path: "+f.getAbsolutePath());
+        System.out.println("length: "+f.length());
+        System.out.println("name:" +f.getName());
+        System.out.println("readable: "+f.canRead());
+
+
+             FileWriter fw = new FileWriter("a.txt");
+        try
+                 {
+                fw.write("  i have created a file.");
+                fw.write( "   priya gopalakrishnan");
+                fw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+ Scanner s =new Scanner(f);
+        while(s.hasNext()){
+            System.out.println("file is read"+s.nextLine());
+        }
+s.close();
+
+    }
+
+}
